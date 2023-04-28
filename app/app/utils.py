cafe08c29958e7a0844f8dfb8f556a96bc689977
Aaron Fraint <aaronfraint@icloud.com>
2023-04-28 12:17:04 -0400

    what day of the week is it?

--- app/app/utils.py
+++ app/app/utils.py
@@ -4,3 +4,13 @@ from pytz import timezone
 
 def get_current_time_est():
     return datetime.now(timezone("US/Eastern"))
+
+
+def get_day_of_week():
+    now = get_current_time_est().weekday()
+    if now < 5:
+        return "Weekday"
+    elif now == 5:
+        return "Satuday"
+    elif now == 6:
+        return "Sunday"
