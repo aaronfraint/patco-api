55c7bb99f5f52565fb8d15b992cf7ccd9feb2244
Aaron Fraint <aaronfraint@icloud.com>
2023-04-28 12:17:19 -0400

    get the currently active service ID

--- app/app/main.py
+++ app/app/main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 
 from .database import sql_query_raw
-from .utils import get_current_time_est
+from .utils import get_current_time_est, get_day_of_week
 
 load_dotenv(find_dotenv())
 
@@ -30,6 +30,31 @@ def get_api_status():
         "status": f"running via automated deployment at {URL_PREFIX}",
         "current_time": get_current_time_est(),
     }
+
+
+@app.get(URL_PREFIX + "/active-service-id/")
+async def active_service_id():
+    now = get_current_time_est()
+    today = str(now.date()).replace("-", "")
+    day_of_week = get_day_of_week()
+    active_service_query = f"""
+        SELECT 
+            service_id,
+            CASE
+                WHEN sunday = 1 THEN 'Sunday'
+                WHEN saturday = 1 THEN 'Saturday'
+                ELSE 'Weekday'
+            END AS day_of_week
+        FROM calendar
+        WHERE end_date >= {today}    
+    """
+    print(active_service_query)
+
+    results = await sql_query_raw(active_service_query, DATABASE_URL)
+
+    for entry in results:
+        if entry["day_of_week"] == day_of_week:
+            return {**entry, "time": now}
 
 
 @app.get(URL_PREFIX + "/foo/")
