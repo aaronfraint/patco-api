4d4e07f8874fa63874575ae200c165d3846426d0
Aaron Fraint <aaronfraint@icloud.com>
2023-05-13 16:30:55 -0400

    remove print

--- app/app/database.py
+++ app/app/database.py
@@ -6,6 +6,7 @@ load_dotenv(find_dotenv())
 
 DATABASE_URL = os.getenv("DATABASE_URL", None)
 
+
 async def sql_query_raw(query: str, uri: str = DATABASE_URL):
     """
     Connect to postgres via `asyncpg` and return raw result of query
@@ -13,7 +14,6 @@ async def sql_query_raw(query: str, uri: str = DATABASE_URL):
     conn = await asyncpg.connect(uri)
 
     try:
-        print("trying...")
         result = await conn.fetch(query)
 
     finally:
