f6e40c79653771ec28188023e0237889c6552112
Aaron Fraint <aaronfraint@icloud.com>
2023-05-13 16:31:06 -0400

    move enums to `options.py`

---
+++ app/app/options.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+
+class Direction(str, Enum):
+    eb = "eb"
+    wb = "wb"
+
+
+class Station(str, Enum):
+    x15_16th_and_locust = "15_16th_and_locust"
+    x12_13th_and_locust = "12_13th_and_locust"
+    x9_10th_and_locust = "9_10th_and_locust"
+    x8th_and_market = "8th_and_market"
+    city_hall = "city_hall"
+    broadway = "broadway"
+    ferry_ave = "ferry_ave"
+    collingswood = "collingswood"
+    westmont = "westmont"
+    haddonfield = "haddonfield"
+    woodcrest = "woodcrest"
+    ashland = "ashland"
+    lindenwold = "lindenwold"
+
+
+class RouteEndpoint(str, Enum):
+    arrive = "arrive"
+    depart = "depart"
